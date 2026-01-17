1553e9cd12e999322494369785a7e8c50196d0ca
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-01-17 18:06:47 +0600

    added add money page

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const addMoney = document.getElementById('add-money');
+if (addMoney) {
+  addMoney.addEventListener('click', function () {
+    window.location.href = "./addmoney.html";
+    console.log("Add money clicked");
+  });
+} 

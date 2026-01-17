5e0c48aff78ff04deab8dbfe4c06c4aeb6185dfa
JabirSiddiqueTalim <jsiddiquetalim@gmail.com>
2026-01-17 13:21:44 +0600

    added check account number and pin

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+
 document.getElementById('login-btn')
-  .addEventListener('click',function()
+  .addEventListener('click',function(event)
 {
-  
+   event.preventDefault();
+   const accountNumber=document.getElementById('account-number').value;
+   const pin=document.getElementById('pin').value;
+   const convertPin=parseInt(pin);
+   if(accountNumber.length==11)
+    {
+      if(convertPin==1234)
+      {
+         window.location.href="./main.html";
+        console.log("pin thik ache ")
+      }else
+      {
+        alert("pin thik nai");
+      }
+    }else
+     {
+      alert("Need valid account number")
+     }
 })
